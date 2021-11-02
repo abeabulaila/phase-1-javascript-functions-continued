331bacf4622caece9432539c04b5873ba4568826
Ibrahim Abulaila <abeabulaila@gmail.com>
2021-11-01 23:02:24 -0400

    commit tree c04de9427248b8fa43722563e0cbc21cf260f0d7

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+function saturdayFun (activity = 'roller-skate'){
+    switch(activity) {
+        default:
+            return(`This Saturday, I want to ${activity}!`);
+}
+}
+saturdayFun();
+
+
+function mondayWork (activity = 'go to the office'){
+    switch(activity) {
+        default:
+            return(`This Monday, I will ${activity}.`);
+}
+}
+mondayWork();
+
+function wrapAdjective(wrapper = "*"){
+    const innerFunction = function (msg){
+        return `You are ${wrapper}${msg}${wrapper}!`
+    
+    }
+    return innerFunction
+}
+
+
+
